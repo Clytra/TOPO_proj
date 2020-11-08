74032178a1e0fce326718f1c3f9ffe0475964699
Clytra <48762967+Clytra@users.noreply.github.com>
2020-11-08 18:45:20 +0100

    Wstępnie zdefiniowany interfejs

---
+++ TopoApp/TopoApp/Models/IClimbingRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopoApp.Models
+{
+    interface IClimbingRepository
+    {
+        IQueryable<AreaModel> Areas { get; }
+        IQueryable<RegionModel> Regions { get; }
+        IQueryable<SectorModel> Sectors { get; }
+        IQueryable<RockModel> Rocks { get; }
+        IQueryable<ClimbingRouteModel> Climbs { get; }
+    }
+}
