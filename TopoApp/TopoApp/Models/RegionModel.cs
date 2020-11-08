48620bd56d2c5687809a9e1458138d9a20044e09
Clytra <48762967+Clytra@users.noreply.github.com>
2020-11-08 13:07:49 +0100

    Utworzenie modelu Region

---
+++ TopoApp/TopoApp/Models/RegionModel.cs
@@ -0,0 +1,15 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopoApp.Models
+{
+    public class RegionModel
+    {
+        public int RegionID { get; set; }
+        public string RegionName { get; set; }
+        public Polygon RegionLoc { get; set; }
+    }
+}
