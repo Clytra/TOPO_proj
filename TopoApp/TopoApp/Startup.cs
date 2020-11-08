e6ba0aaba4eba4e93405a301745458f772e2bdd4
Clytra <48762967+Clytra@users.noreply.github.com>
2020-11-08 19:44:51 +0100

    Rejestrowanie usługi repozytorium

--- TopoApp/TopoApp/Startup.cs
+++ TopoApp/TopoApp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TopoApp.Models;
 
 namespace TopoApp
 {
@@ -16,6 +17,7 @@ namespace TopoApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IClimbingRepository, FakeClimbingRepository>();
             services.AddMvc();
         }
 
