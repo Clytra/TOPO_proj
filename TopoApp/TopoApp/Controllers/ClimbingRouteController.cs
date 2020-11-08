89f6aab2cc226e32bcb0f666077607259ee65d5d
Clytra <48762967+Clytra@users.noreply.github.com>
2020-11-08 19:49:51 +0100

    Dodanie kontrolera

---
+++ TopoApp/TopoApp/Controllers/ClimbingRouteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TopoApp.Models;
+
+namespace TopoApp.Controllers
+{
+    public class ClimbingRouteController : Controller
+    {
+        private IClimbingRepository repository;
+        public ClimbingRouteController(IClimbingRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List() => View(repository.Areas);
+    }
+}
