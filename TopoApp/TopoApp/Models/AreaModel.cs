48102168bc75d86221eabcd3d6e69a349043a53b
Clytra <48762967+Clytra@users.noreply.github.com>
2020-11-08 12:56:36 +0100

    Model Area + dodanie biblioteki NetTopologySuite + Moq

---
+++ TopoApp/TopoApp/Models/AreaModel.cs
@@ -0,0 +1,16 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopoApp.Models
+{
+    public class AreaModel
+    {
+        public int AreaID { get; set; }
+        public string AreaName { get; set; }
+
+        public Polygon AreaLoc { get; set; }
+    }
+}
