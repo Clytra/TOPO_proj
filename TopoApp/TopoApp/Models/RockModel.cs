785a86d0d58184d3b9a7a82fb4c92c720eabe377
Clytra <48762967+Clytra@users.noreply.github.com>
2020-11-08 13:28:05 +0100

    Utworzenie modelu Rock

---
+++ TopoApp/TopoApp/Models/RockModel.cs
@@ -0,0 +1,20 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopoApp.Models
+{
+    public class RockModel
+    {
+        public int RockID { get; set; }
+        public string RockName { get; set; }
+        public string RockType { get; set; }
+        public int NumOfRoutes { get; set; }
+        public double MinHight { get; set; }
+        public double MaxHight { get; set; }
+        public bool ChildrenFriendly { get; set; }
+        public LineString RockLoc { get; set; }
+    }
+}
