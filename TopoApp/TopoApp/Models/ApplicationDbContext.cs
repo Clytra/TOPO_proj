e680aeaba8a4734e40e2764ccda111e659f03879
Clytra <48762967+Clytra@users.noreply.github.com>
2020-11-15 18:49:42 +0100

    Utworzenie klas bazy danych

---
+++ TopoApp/TopoApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TopoApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<AreaModel> Areas { get; set; }
+        public DbSet<RegionModel> Regions { get; set; }
+        public DbSet<SectorModel> Sectors { get; set; }
+        public DbSet<RockModel> Rocks { get; set; }
+        public DbSet<ClimbingRouteModel> ClimgingRoutes { get; set; }
+    }
+}
