736ee41bb59e4fae5355fd0c74ec6f75a9ceda64
Clytra <48762967+Clytra@users.noreply.github.com>
2020-11-15 18:55:46 +0100

    Utworzenie klasy repozytorium

---
+++ TopoApp/TopoApp/Models/EFClimbingRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopoApp.Models
+{
+    public class EFClimbingRepository : IClimbingRepository
+    {
+        private ApplicationDbContext _context;
+        public EFClimbingRepository(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+        public IQueryable<AreaModel> Areas => _context.Areas;
+
+        public IQueryable<RegionModel> Regions => _context.Regions;
+
+        public IQueryable<SectorModel> Sectors => _context.Sectors;
+
+        public IQueryable<RockModel> Rocks => _context.Rocks;
+
+        public IQueryable<ClimbingRouteModel> ClimbingRoutes => _context.ClimgingRoutes;
+    }
+}
